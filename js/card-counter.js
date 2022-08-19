bfc63b8ecd19fed85cd48f7fe4ea919051f2c564
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-08-19 16:20:24 -0500

    cc

--- js/card-counter.js
+++ js/card-counter.js
@@ -2,7 +2,37 @@
 
 let count = 0;
 
+function cc(cards) {
 
-function cc(cards){
+    switch (cards) {
+        case(2):
+        case(3):
+        case(4):
+        case(5):
+        case(6):
+            count++
+            break;
+        case(10):
+        case("J"):
+        case("Q"):
+        case("K"):
+        case("A"):
+            count--
+            break;
+    }
 
+    if (count > 0) {
+        console.log(count + " Bet")
+        return count + " Bet";
+    } else {
+        console.log(count + " Bet")
+        return count + " Bet";
+    }
 }
+
+
+cc(3);
+cc(7);
+cc("A");
+cc("Q");
+cc(4)
