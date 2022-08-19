dbbdc19db0bd7c7035c093243b6a3c52bce66af6
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-08-19 18:44:09 -0500

    find substring and remove

---
+++ js/findAllSubStrings.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+function findSubstring(str){
+
+    switch(str){
+        case(str):
+            str = str.replaceAll('the cat is fat', '')
+            break;
+    }
+
+    console.log(str)
+}
+
+findSubstring("the cat is fat and the dog is skinny");
+findSubstring("dog cat dog the cat is fat cat dog the is the cat is dog")
