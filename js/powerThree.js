05ae1a4a28fb4dc30f2b94f5b9c09a202b8b3486
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-08-24 12:32:57 -0500

    refactored powerThree

--- js/powerThree.js
+++ js/powerThree.js
@@ -1,11 +1,18 @@
 const isPowerOfThree = function (n) {
-    if (n === 0) {
-        return false;
-    } else if (n === Math.pow(3, Math.round(Math.log(n) / Math.log(3)))) {
-        return true;
-    } else {
-        return false
-    }
 
+
+    //TODO: give 'n' a value on the line below and uncomment line
+
+    // n =
+    if (n === 0) {
+        console.log(false)
+        return false;
+    } else if(n === Math.pow(3, Math.round(Math.log(n) / Math.log(3)))) {
+        console.log(true)
+        return true;
+    }else {
+        console.log(false)
+        return false;
+    }
 };
 isPowerOfThree();
